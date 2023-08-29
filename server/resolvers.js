a6f66668b0532cc33fcef04af67f96b4884daa11
ptlnik13 <nikeshvpatel@gmail.com>
2023-08-29 09:50:39 -0400

    DB access

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,7 @@
+import {getJobs} from "./db/jobs.js";
+
 export const resolvers = {
     Query: {
-        jobs: () => [{
-            id         : 'test-id-1',
-            title      : 'The Title 1',
-            description: 'The Description 1'
-        },
-            {
-                id         : 'test-id-2',
-                title      : 'The Title 2',
-                description: 'The Description 2'
-            }]
+        jobs: () => getJobs()
     }
 }
