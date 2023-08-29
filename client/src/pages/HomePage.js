bea5f37600b695771662fa2844a9e4b34dbf6bf1
ptlnik13 <nikeshvpatel@gmail.com>
2023-08-29 14:09:38 -0400

    graphql request

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,18 @@
 import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import {jobs} from '../lib/fake-data';
+import {getJobs} from "../lib/graphql/queries";
+
+getJobs().then(data => console.log(data));
 
 function HomePage() {
-  return (
-    <div>
-      <h1 className="title">
-        Job Board
-      </h1>
-      <JobList jobs={jobs} />
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="title">
+                Job Board
+            </h1>
+            <JobList jobs={jobs}/>
+        </div>
+    );
 }
 
 export default HomePage;
