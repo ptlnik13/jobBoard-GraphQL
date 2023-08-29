c1e584e0161180416f7e4647de17e19e633bed08
ptlnik13 <nikeshvpatel@gmail.com>
2023-08-29 12:30:27 -0400

    Object Associations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,5 @@
 import {getJobs} from "./db/jobs.js";
+import {getCompany} from "./db/companies.js";
 
 export const resolvers = {
     Query: {
@@ -6,7 +7,8 @@ export const resolvers = {
     },
     Job  : {
         title: (job) => job.title,
-        date : (job) => toIsoDate(job.createdAt)
+        date : (job) => toIsoDate(job.createdAt),
+        company: ({companyId})=> getCompany(companyId)
     }
 }
 
