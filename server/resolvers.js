ab6b06b3d307a85510a3be3ad3e8b3e19a2ff2f5
ptlnik13 <nikeshvpatel@gmail.com>
2023-08-29 11:08:13 -0400

    Field Resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,5 +3,12 @@ import {getJobs} from "./db/jobs.js";
 export const resolvers = {
     Query: {
         jobs: () => getJobs()
+    },
+    Job:{
+        date:(job)=> toIsoDate(job.createdAt)
     }
+}
+
+function toIsoDate(value){
+    return value.slice(0, 'yyyy-mm-dd'.length)
 }
