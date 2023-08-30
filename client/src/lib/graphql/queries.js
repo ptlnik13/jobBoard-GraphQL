bc6dc7b8cce15a8e445829e7aa35437b3101b6c9
ptlnik13 <nikeshvpatel@gmail.com>
2023-08-30 12:14:26 -0400

    query Variables

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -19,3 +19,22 @@ export async function getJobs() {
     const {jobs} = await client.request(query)
     return jobs;
 }
+
+export async function getJob(id) {
+    const query = gql`
+        query job($id: ID!) {
+            job(id: $id){
+                id
+                date
+                title
+                company{
+                    id
+                    name
+                }
+                description
+            }
+        }
+    `
+    const {job} = await client.request(query, {id})
+    return job;
+}
