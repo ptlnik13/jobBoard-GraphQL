cbfbd45177377fb37ab3291929fdf64c8d85f8d4
ptlnik13 <nikeshvpatel@gmail.com>
2023-09-05 14:02:09 -0400

    Apollo client setup

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -1,4 +1,5 @@
-import {GraphQLClient, gql} from "graphql-request";
+import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
+import {GraphQLClient} from "graphql-request";
 import {getAccessToken} from "../auth";
 
 const client = new GraphQLClient('http://localhost:9000/graphql', {
@@ -10,6 +11,11 @@ const client = new GraphQLClient('http://localhost:9000/graphql', {
         return {};
     }
 });
+
+const apolloClient = new ApolloClient({
+    uri  : 'http://localhost:9000/graphql',
+    cache: new InMemoryCache(),
+})
 
 export async function getJobs() {
     const query = gql`
